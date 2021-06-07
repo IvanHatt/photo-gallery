d9fac4393bf005239025d5e6c5da881d2dfead14
Ivan Hattemer <ivanhattemer@gmail.com>
2021-06-07 16:14:06 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css'
 import Header from './Components/Header'
 import Upload from './Components/Upload'
+import ImageGrid from './Components/ImageGrid'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Upload />
+      <ImageGrid />
     </div>
   )
 }
