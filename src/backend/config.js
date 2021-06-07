6e648a2815513c26dd4813f852166fc740619f6e
Ivan Hattemer <ivanhattemer@gmail.com>
2021-06-07 14:23:40 +0300

    Add add firebase config

---
+++ src/backend/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyAj1Ylli9IH-6megtfa9ikEdjtfRxt-1G4',
+  authDomain: 'photo-gallery-fba23.firebaseapp.com',
+  projectId: 'photo-gallery-fba23',
+  storageBucket: 'photo-gallery-fba23.appspot.com',
+  messagingSenderId: '314363516778',
+  appId: '1:314363516778:web:4f420f5d4ce79ce2337713',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore, projectStorage }
