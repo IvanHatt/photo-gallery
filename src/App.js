5afc9738763e82eb1d42e6b5f08dde167044b1b2
Ivan Hattemer <ivanhattemer@gmail.com>
2021-06-08 14:23:33 +0300

    Add add animations with framer motion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import './index.css'
 import Header from './Components/Header'
 import Upload from './Components/Upload'
 import ImageGrid from './Components/ImageGrid'
+import Modal from './Components/Modal'
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null)
+
   return (
     <div className='App'>
       <Header />
       <Upload />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   )
 }
